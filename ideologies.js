d252c0e67571b26b6c0783746597e689c5da4a08
Denomin8 <61699737+Denomin8@users.noreply.github.com>
2020-03-02 15:16:19 +0000

    Add files via upload
    Files are edited and translated into the Thai language from the original works of 8values at https://github.com/8values/8values.github.io

---
+++ ideologies.js
@@ -0,0 +1,470 @@
+ideologies = [
+    {
+        "name": "ลัทธิอนาธิปไตย-คอมมิวนิสต์ (Anarcho-Communism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 90
+        }
+    },
+    {
+        "name": "คอมมิวนิสต์อิสรนิยม (Libertarian Communism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ลัทธิทรอตสกี้ (Trotskyism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ลัทธิมากซ์ (Marxism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 40,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ลัทธิเดอเลออน (De Leonism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ลัทธิเลนิน (Leninism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 40,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "ลัทธิสตาลิน/เหมา (Stalinism/Maoism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 60
+        }
+    },
+    {
+        "name": "คอมมิวนิสต์แบบนับถือศาสนา (Religious Communism)",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {
+        "name": "สังคมนิยมรัฐ (State Socialism)",
+        "stats": {
+            "econ": 80,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "สังคมนิยมแบบเทวาธิปไตย (Theocratic Socialism)",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "สังคมนิยมแบบนับถือศาสนา (Religious Socialism)",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 70,
+            "scty": 20
+        }
+    },
+    {
+        "name": "สังคมนิยมแบบประชาธิปไตย (Democratic Socialism)",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ปฏิวัติสังคมนิยม (Revolutionary Socialism)",
+        "stats": {
+            "econ": 80,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "สังคมนิยมแบบอิสรนิยม (Libertarian Socialism)",
+        "stats": {
+            "econ": 80,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "สหการอนาธิปไตย (Anarcho-Syndicalism)",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ประชานิยมฝ่ายซ้าย (Left-Wing Populism)",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "เทวธรรมาธิปไตยแบบคาธอลิค (Theocratic Distributism)",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "ธรรมาธิปไตยแบบคาธอลิค (Distributism)",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 20
+        }
+    },
+    {
+        "name": "เสรีนิยมทางสังคม/สังคมนิยมเสรี (Social Liberalism)",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ประชาธิปไตยแบบคริสต์ (Christian Democracy)",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {
+        "name": "ประชาธิปไตยสังคมนิยม (Social Democracy)",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "พิพัฒนนิยม (Progressivism)",
+        "stats": {
+            "econ": 60,
+            "dipl": 80,
+            "govt": 60,
+            "scty": 100
+        }
+    },
+    {
+        "name": "อนาธิปไตยแบบเกื้อกูลนิยม (Anarcho-Mutualism)",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 70
+        }
+    },
+    {
+        "name": "เผด็จการเบ็ดเสร็จนิยมแบบชาตินิยม (National Totalitarianism)",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "เผด็จการเบ็ดเสร็จนิยมแบบทั้งโลก (Global Totalitarianism)",
+        "stats": {
+            "econ": 50,
+            "dipl": 80,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "คตินิยมนักวิชาการ (Technocracy)",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "สายกลาง (Centrist)",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "เสรีนิยม (Liberalism)",
+        "stats": {
+            "econ": 50,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 60
+        }
+    },
+    {
+        "name": "อนาธิปไตยแบบนับถือศาสนา (Religious Anarchism)",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 20
+        }
+    },
+    {
+        "name": "ประชานิยมฝ่ายขวา (Right-Wing Populism)",
+        "stats": {
+            "econ": 40,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {
+        "name": "อนุรักษ์นิยมสายกลาง (Moderate Conservatism)",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {
+        "name": "กลุ่มปฎิกิริยาหรือปฏิการณ์ (Reactionary)",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 10
+        }
+    },
+    {
+        "name": "อิสรนิยมแบบสังคมนิยม (Social Libertarianism)",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 70
+        }
+    },
+    {
+        "name": "อิสรนิยม (Libertarianism)",
+        "stats": {
+            "econ": 40,
+            "dipl": 60,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {
+        "name": "อนาธิปไตยแบบอัตตานิยม (Anarcho-Egoism)",
+        "stats": {
+            "econ": 40,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "นาซี (Nazism)",
+        "stats": {
+            "econ": 40,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "name": "อัตตาธิปไตย (Autocracy)",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "ฟาสซิสต์ (Fascism)",
+        "stats": {
+            "econ": 40,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+        "name": "ฟาสซิสต์แบบทุนนิยม (Capitalist Fascism)",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+        "name": "อนุรักษ์นิยม (Conservatism)",
+        "stats": {
+            "econ": 30,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {
+        "name": "เสรีนิยมใหม่ (Neo-Liberalism)",
+        "stats": {
+            "econ": 30,
+            "dipl": 30,
+            "govt": 50,
+            "scty": 60
+        }
+    },
+    {
+        "name": "เสรีนิยมดั้งเดิม (Classical Liberalism)",
+        "stats": {
+            "econ": 30,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "ทุนนิยมแบบอำนาจนิยม (Authoritarian Capitalism)",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 20,
+            "scty": 40
+        }
+    },
+    {
+        "name": "ทุนนิยมรัฐ (State Capitalism)",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 50
+        }
+    },
+    {
+        "name": "อนุรักษ์นิยมใหม่ (Neo-Conservatism)",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {
+        "name": "มูลฐานนิยม (Fundamentalism)",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 5
+        }
+    },
+    {
+        "name": "ทุนนิยมแบบอิสรนิยม (Libertarian Capitalism)",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {
+        "name": "อนาธิปไตยแบบตลาดเสรี (Market Anarchism)",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "วัตถุวิสัยนิยม (Objectivism)",
+        "stats": {
+            "econ": 10,
+            "dipl": 50,
+            "govt": 90,
+            "scty": 40
+        }
+    },
+    {
+        "name": "ทุนนิยมแบบเผด็จการเบ็ดเสร็จ (Totalitarian Capitalism)",
+        "stats": {
+            "econ": 0,
+            "dipl": 30,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "ทุนนิยมล้นเกิน (Ultra-Capitalism)",
+        "stats": {
+            "econ": 0,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "ทุนนิยมอนาธิปไตย (Anarcho-Capitalism)",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    }
+];
